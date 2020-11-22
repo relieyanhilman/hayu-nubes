a52ece49474fdfe8eb73dc3ddf15ed00aa634d00
David M <68513396+davidmoelia@users.noreply.github.com>
2020-11-22 17:57:28 +0700

    Add files via upload

---
+++ queue.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "header_tubes.c"
+
+void Alokasi(address *P, ElType X){
+    *P = (address) malloc(sizeof(Node));
+    Info(*P) = X;
+    Next(*P) = Nil;
+}
+
+void Dealokasi(address P){
+    free(P);
+}
+
+boolean IsEmpty(Queue Q){
+    return ((Head(Q) == Nil) && (Tail(Q) == Nil));
+}
+
+void CreateEmpty(Queue *Q){
+    Head(*Q) = Nil;
+    Tail(*Q) = Nil;
+}
+
+void Enqueue(Queue *Q, ElType X){
+    address P;
+    Alokasi (&P,X);
+    if (P!=Nil){
+        if (IsEmpty(*Q)){
+            Head(*Q) = P;
+        } else {
+            Next(Tail(*Q)) = P;
+        }
+        Tail(*Q) = P;
+    }
+}
+
+void Dequeue(Queue *Q, ElType *X){
+    address P;
+    *X = InfoHead(*Q);
+    P = Head(*Q);
+    Head(*Q) = Next(Head(*Q));
+    if (Head(*Q) == Nil) {
+        Tail(*Q) = Nil;
+    }
+    Next(P) = Nil;
+    Dealokasi(P);
+}
+
+
